fc6adff8ae65a34c5de2cdb2dd7df81b20714267
loloadly <marillaadly@gmail.com>
2021-01-22 18:51:11 +0200

    news&reporterindex

--- task-3/src/home.js
+++ task-3/src/home.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const News = require('./models/news')
+const Reporter = require('./models/reporter')
 const newsRoute = require('./routers/new-route')
+const reporterRoute = require('./routers/reporter-route')
 require('./db/mongoose')
 const app = express()
 app.use(express.json())
 app.use(newsRoute)
+app.use(reporterRoute)
 const port = 3000
 
 app.listen(port,()=>console.log('Server is ON'))
